50871cd13f39ca47c197760579ca85f66f71ce83
Mike Stevens <mesgraphix@gmail.com>
2019-01-17 17:39:33 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,56 @@
+# adapt-notify-imagegrid
+
+**Notifyimagegrid** is a *presentation component* for the [Adapt framework](https://github.com/adaptlearning/adapt_framework) based on [adapt-hotgrid](https://github.com/cgkineo/adapt-hotgrid). It displays a set of images in a grid layout, but without interactivity. 
+
+This can be useful for visual bullet lists where a set of labelled images is required but no further information necessary.
+
+![imagegrid](https://raw.githubusercontent.com/chrisgillison/shared-assets/master/imagegrid.gif)   
+
+## Installation
+This component must be manually installed.
+
+## Settings Overview
+
+The attributes listed below are used in *components.json* to configure **Notifyimagegrid**, and are properly formatted as JSON in [*example.json*](https://github.com/chrisgillison/adapt-imagegrid/blob/master/example.json). 
+
+### Attributes
+
+[**core model attributes**](https://github.com/adaptlearning/adapt_framework/wiki/Core-model-attributes): These are inherited by every Adapt component. [Read more](https://github.com/adaptlearning/adapt_framework/wiki/Core-model-attributes).
+
+**_component** (string): This value must be: `notify-imagegrid`. (One word.)
+
+**\_classes** (string): CSS class name to be applied to **Notifyimagegrid**’s containing `div`. The class must be predefined in one of the Less files. Separate multiple classes with a space.
+
+**\_layout** (string): This defines the horizontal position of the component in the block. Acceptable values are `full`, `left` or `right`.
+
+**\_columns** (number): This value determines the number of columns within the grid. Any number of columns can be set however keep in mind the more columns there are the smaller the items will be.
+
+Notifyimagegrid has a dynamic layout system. If you have 5 items but set the columns to 3, notify-imagegrid will put 3 items in the first row and 2 on the second. The second row then will be automatically centred. This works with any amount of items and columns - ie that last row will always be centred for you.
+
+**\_items** (string): Multiple items may be created. Each item represents one grid item for this component and contains values for **title**, **body**, **\_graphic** and **\_itemGraphic**. 
+
+>**\_graphic** (string): This is the image that displays for each grid item. This graphic requires three state **src** values, with additional values **alt** and **title**.
+
+>>**src** (string): File name (including path) of the image. Path should be relative to the *src* folder (e.g., *course/en/images/c-15.png*).
+
+>>**alt** (string): This text becomes the image’s `alt` attribute.
+
+>>**title** (string): This is optional text which is displayed under the grid item image.
+
+>**\_itemClasses** (string): Allows for individual styling of each grid item.
+
+
+### Accessibility
+**Notifyimagegrid** has a label assigned using the [aria-label](https://github.com/adaptlearning/adapt_framework/wiki/Aria-Labels) attribute: **ariaRegion**. These labels are not visible elements. They are utilized by assistive technology such as screen readers. This label is included within the *example.json* and will need adding to the _globals in *course.json*.
+
+## Limitations
+Currently not tested in Adapt Authoring Tool.
+Notifyimagegrid automatically switches to 2 columns in mobile mode for the best user experience however this can be overridden in the css.
+
+----------------------------
+**Version number:**  1.0.0
+**Framework versions:**  2.0     
+**Author / maintainer:**   
+**Accessibility support:** WAI AA   
+**RTL support:** yes  
+**Cross-platform coverage:** Chrome, Chrome for Android, Firefox (ESR + latest version), Edge, IE11, IE Mobile 11, Safari 10+11 for macOS+iOS, Opera 
---
+++ js/adapt-imagegrid.js
@@ -0,0 +1,151 @@
+
+define([
+    'core/js/adapt',
+    'core/js/views/componentView'
+], function(Adapt, ComponentView) {
+
+    var Notifyimagegrid = ComponentView.extend({
+
+        preRender: function () {
+            this.listenTo(Adapt, 'device:changed', this.resizeControl);
+
+            this.setDeviceSize();
+            this.checkIfResetOnRevisit();
+        },
+
+        events: function() {
+            return Adapt.device.touch == true ? {
+                'inview':                       'inview',
+                'click img' : 'gridnotifyPopup'
+            } : {
+                'inview':                       'inview',
+                'click img' : 'gridnotifyPopup'
+            }
+        },
+
+        setDeviceSize: function() {
+            if (Adapt.device.screenSize === 'large') {
+                this.$el.addClass('desktop').removeClass('mobile');
+                this.model.set('_isDesktop', true);
+            } else {
+                this.$el.addClass('mobile').removeClass('desktop');
+                this.model.set('_isDesktop', false);
+            }
+        },
+
+        checkIfResetOnRevisit: function() {
+            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+
+            // If reset is enabled set defaults
+            if (isResetOnRevisit) {
+                this.model.reset(isResetOnRevisit);
+            }
+        },
+
+        inview: function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                if (visiblePartY === 'top') {
+                    this._isVisibleTop = true;
+                } else if (visiblePartY === 'bottom') {
+                    this._isVisibleBottom = true;
+                } else {
+                    this._isVisibleTop = true;
+                    this._isVisibleBottom = true;
+                }
+
+                if (this._isVisibleTop && this._isVisibleBottom) {
+                    this.$('.component-widget').off('inview');
+                    //this.setCompletionStatus();
+                    this.completePopup();
+                }
+
+            }
+        },
+
+        remove: function() {
+          // Remove any 'inview' listener attached.
+          this.$('.component-widget').off('inview');
+
+          ComponentView.prototype.remove.apply(this, arguments);
+        },
+
+        postRender: function() {
+            this.setUpColumns();
+
+            this.$('.notify-imagegrid-widget').imageready(_.bind(function() {
+                this.setReadyStatus();
+
+                // Bind 'inview' once the image is ready.
+                this.$('.component-widget').on('inview', _.bind(this.inview, this));
+
+            }, this));
+
+            this.reverseDirection();
+            this.constrainHeight();
+        },
+
+        resizeControl: function() {
+            this.setDeviceSize();
+            this.render();
+        },
+
+        reverseDirection: function() {
+
+            if (this.model.get('_reverseDirection') && this.model.get('_isEnabled')) {
+                this.$(".imagegrid-grid-inner").addClass("reverserow");
+            }
+        },
+
+        constrainHeight: function() {
+            var rowheight = this.model.get('_gridrowHeight')._heightAmount;
+
+            if ( this.model.get('_gridrowHeight')._constrainHeight && this.model.get('_isEnabled')) {
+                this.$(".notify-imagegrid-item-image img").addClass("constrainimg");
+                this.$(".constrainimg").css({"max-height": rowheight + 'px',"min-height": rowheight + 'px'});
+            }
+        },
+
+        setUpColumns: function() {
+            var columns = this.model.get('_columns');
+
+            if (columns && Adapt.device.screenSize === 'large') {
+                var griditempercent = (100 / columns);
+                var gridminuspad = (5 / columns);
+                this.$('.imagegrid-grid-item').css('width', griditempercent + '%');
+            }
+        },
+
+        completePopup: function () {
+            var mycurrentid = '.' + this.model.get('_id');
+            var howmanygridimg = $(mycurrentid + " .howmanygrid").length;
+            var howmanystr = $(mycurrentid + " .totalgrid").text();
+            
+            if (howmanystr >= howmanygridimg ) {
+                this.setCompletionStatus();
+            } else if (howmanygridimg == 0) {
+                this.setCompletionStatus();
+            }
+        }, 
+
+        gridnotifyPopup: function (event) {
+            event.preventDefault();
+
+            this.model.set('_active', false);
+
+            var mycurrentid = '.' + this.model.get('_id');
+            var howmanygridimg = $(mycurrentid + " .howmanygrid").length;
+            var howmanystr = $(mycurrentid + " .totalgrid").text();
+            
+            if (howmanystr >= howmanygridimg ) {
+                this.setCompletionStatus();
+            } else if (howmanygridimg == 0) {
+                this.setCompletionStatus();
+            }
+        }
+
+    },{
+        template: "notify-imagegrid"
+    });
+
+    return Adapt.register("notify-imagegrid", Notifyimagegrid);
+});
